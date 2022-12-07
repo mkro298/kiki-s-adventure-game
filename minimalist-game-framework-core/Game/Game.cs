2d7e49342a09978e283f6d0b6f2895c37d21ada8
ANNABELLE MARTIN <s-ANNMARTIN@lwsd.org>
2022-12-06 16:28:07 -0800

    Fixed bugs, make it run smoother

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -6,45 +6,46 @@ class Game
 {
     public static readonly string Title = "Minimalist Game Framework"; 
     public static readonly Vector2 Resolution = new Vector2(1275, 750);
-    readonly Texture background = Engine.LoadTexture("Kirby red level background.png");
+    public static int speed = 5;
+    int scroll = 0;
     readonly Font font = Engine.LoadFont("font.ttf", 20);
     readonly Texture background = Engine.LoadTexture("Kirby red level background.png");
     static int numBlocks = 202;
-    Block[] blocks;
-    int scroll = 0;
-    Player b = new Player();
+    static Block[] blocks;
+    Player player = new Player(blocks);
     
     public Game()
     {
         Engine.DrawTexture(background, Vector2.Zero);
         reload();
-        Enemy c = new Enemy(0, b);
+        Enemy enemy = new Enemy(0, player);
     }
     public void Update()
     {
+        // reload the files and textures if r is pressed
         Engine.DrawTexture(background, Vector2.Zero);
         if (Engine.GetKeyDown(Key.R))
         {
             reload();
         }
-        
-        b.Update(); 
-        
-        for (int i = 0; i < blocks.Length; i++)
-        { 
-            blocks[i].draw(scroll);
+
+        // update the player
+        player.Update(scroll);
+
+        //adjust scroll
+        if (Engine.GetKeyHeld(Key.Right) && player.getKPosition().X > 940 && scroll > -7425 && player.getMoveRight())
+        {
+            scroll -= speed;
         }
-
-        int speed = 5;
-
-        if (Engine.GetKeyHeld(Key.Left) && scroll <= -1)
+        if (Engine.GetKeyHeld(Key.Left) && player.getKPosition().X < 255 && scroll < 0 && player.getMoveLeft())
         {
             scroll += speed;
         }
 
-        if (Engine.GetKeyHeld(Key.Right) && scroll >= -7425)
+        // draw the blocks
+        for (int i = 0; i < blocks.Length; i++)
         {
-            scroll -= speed;
+            blocks[i].draw(scroll);
         }
     }
 
@@ -60,6 +61,7 @@ class Game
 
             blocks[i] = new Block(Int32.Parse(nums[0]), Int32.Parse(nums[1]), Int32.Parse(nums[2]));
         }
+        player.updateBlocks(blocks);
         sr.Close();
     }
 
